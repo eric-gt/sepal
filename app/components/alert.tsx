0e1d44bc0fa01a698111c960a8c5b8a25c2f754a
Eric GT <bookjunkie93@gmail.com>
2022-04-17 16:52:20 -0400

    add new plants to the feed!

--- app/components/alert.tsx
+++ app/components/alert.tsx
@@ -1,6 +1,6 @@
 export default function Alert({text}: any) {
     return (
-        <div className="align-middle text-center bg-red-500">
+        <div className="align-middle text-center rounded-full py-px bg-red-500">
             <p className="text-sm md:text-base antialiased">{text}</p>
         </div>
     );
