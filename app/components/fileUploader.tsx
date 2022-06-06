05b91af1ace8465d7c8517c7ec0752f765d040fa
Eric GT <bookjunkie93@gmail.com>
2022-06-05 20:05:48 -0400

    ad image preview

--- app/components/fileUploader.tsx
+++ app/components/fileUploader.tsx
@@ -1,16 +1,61 @@
-import { useRef } from "react";
-export default function FileUploader({onFileSelect}) {
-    const fileInput = useRef(null)
+import Image from "next/image";
+import { useState, useEffect, useRef } from "react";
+export default function FileUploader({ onFileSelect }) {
+  const fileInput = useRef(null);
+  const [selectedFile, setSelectedFile] = useState();
+  const [preview, setPreview] = useState();
 
-    const handleInput = (e) => {
-        e.preventDefault();
-        onFileSelect(e.target.files[0])
+  useEffect(() => {
+    if (!selectedFile) {
+      setPreview(undefined);
+      return;
     }
+    const objUrl = URL.createObjectURL(selectedFile);
+    setPreview(objUrl);
+    return () => URL.revokeObjectURL(objUrl);
+  }, [selectedFile]);
 
-    return(
-        <div className="file-uploader">
-            <input type="file" onChange={handleInput} />
-            <button onClick={e => fileInput.current && fileInput.current.click()} />
-        </div>
-    )
+  const handleInput = (e) => {
+    e.preventDefault();
+    if (e.target.files.length > 0) {
+      //prevent previously selected images from being unloaded
+      setSelectedFile(e.target.files[0]);
+      onFileSelect(e.target.files[0]);
+    }
+  };
+
+  return (
+    <div className="file-uploader max-h-14 max-w-14">
+      <input
+        type="file"
+        accept=".jpg,.png,.jpeg"
+        capture="environment"
+        ref={fileInput}
+        id="plant-file-input"
+        onChange={handleInput}
+        className="hidden"
+      />
+      <label htmlFor="plant-file-input">
+        {preview && (
+          <Image
+            className="hover:cursor-pointer"
+            alt="a lovely photo of your plant"
+            src={preview}
+            width="64px"
+            height="64px"
+          />
+        )}
+        {!preview && (
+          <div>
+            <div className="text-center border-dashed border-2 border-indigo-400 hover:border-indigo-600 hover:cursor-pointer rounded-lg w-14 h-14">
+              <p className="mt-3 text-lg font-extrabold text-slate-400 hover:text-slate-600">
+                +
+              </p>
+            </div>
+            <p>Upload a picture of your plant!</p>
+          </div>
+        )}
+      </label>
+    </div>
+  );
 }
--- app/components/plantCard/plantCard.tsx
+++ app/components/plantCard/plantCard.tsx
@@ -1,8 +1,33 @@
-import { useState } from 'react';
-import Image from 'next/image';
-import {Alert, CardFeed} from '../index'
-export default function PlantCard(props: any) {
-    const card = `
+import { useState } from "react";
+import Image from "next/image";
+import { Alert, CardFeed } from "../index";
+
+export interface PlantData {
+  id: string;
+  alert: boolean;
+  thumb: string;
+  sciName: string;
+  nickname: string;
+  room: string;
+  instructions: {
+    water: {
+      amount: string;
+      interval: number;
+    };
+    soil: string;
+    light: string;
+    humidity: string;
+    temperature: number;
+  };
+}
+export default function PlantCard({
+  data,
+  expanded,
+}: {
+  data: PlantData;
+  expanded: boolean;
+}) {
+  const card = `
         mx-2
         mb-4
         pl-4
@@ -20,70 +45,87 @@ export default function PlantCard(props: any) {
         w-80
         md:w-screen
         max-w-screen-sm
-    `
-    const [isExpanded, setIsExpanded] = useState(props.expanded);
-    
-        
-    const abbrev = (sciName: string): string => {
-        let names = sciName.split(" ");
-        names[0] = `${names[0].split("")[0]}.`;
-        return names.join(" ");
-    } 
-    const toggleExpanded = () => {
-        setIsExpanded(!isExpanded)
-    }
-    const forceExpanded = (val: boolean): void => {
-        setIsExpanded(val);
-    }
-    return (
-        <div onClick={toggleExpanded} className={card}>
-            {props.alert && <div className="grid grid-cols-3">
-                <div></div>
-                <div></div>
-                <Alert text="Needs Water!"/>
-            </div>}
-            <div className = "flex flex-row box-border pr-2 mt-2 space-x-3 ">
-                <Image
-                    className="basis-16 object-cover"
-                    width="64"
-                    height="64"
-                    src={props.thumb}
-                    alt={"a thumbnail with a photo of your plant!"}
-                /> 
-                {isExpanded && <div id={`${props.id}-main-info`} className="basis-auto ">
-                    <p>Scientific Name: <i>{props.sciName}</i></p>
-                    <p>Nickname: {props.nickname}</p>
-                    <p>Room: {props.room}</p>
-                </div>}
-                {!isExpanded && <div id={`${props.id}-collapsed-view`} className="align-bottom text-center space-y-1">
-                    <p className="mt-4 md:mt-2">{props.nickname}</p>
-                    <p className="italic">({abbrev(props.sciName)})</p>
-                </div>}
+    `;
+  const [isExpanded, setIsExpanded] = useState(expanded);
 
-            </div>
-            {isExpanded && <div id={`${props.id}-instructions`} className="py-4 pr-4 ">
-                <p className="pb-2">Watering: {props.instructions.water}</p>
-                <p className="pb-2">Light: {props.instructions.light}</p>
-            </div>}
-            {isExpanded && <div id={`${props.id}-feeds`} className="grid gap-2 grid-cols-2 pr-4">
-                <CardFeed
-                    title="Last Watered"
-                    buttonText="Water"
-                    data ={[
-                        {text:"Eric", date:"April 4th", key: 0},
-                        {text:"Despina", date:"March 20th", key: 1},
-                        {text:"Flannery", date:"March 6th", key:2}
-                    ]}
-                />
-                <CardFeed
-                    title="Observations"
-                    buttonText="Write Note"
-                    data={[
-                        {text: "Our child is thriving ~Eric", date: "April 4th", key: 0},
-                        {text: "Do your dishes ~Despina", date: "March 20th", key: 1}
-                    ]}
-                />
-            </div>}
+  const abbrev = (sciName: string): string => {
+    let names = sciName.split(" ");
+    names[0] = `${names[0].split("")[0]}.`;
+    return names.join(" ");
+  };
+  const toggleExpanded = () => {
+    setIsExpanded(!isExpanded);
+  };
+  const forceExpanded = (val: boolean): void => {
+    setIsExpanded(val);
+  };
+  return (
+    <div onClick={toggleExpanded} className={card}>
+      {data.alert && (
+        <div className="grid grid-cols-3">
+          <div></div>
+          <div></div>
+          <Alert text="Needs Water!" />
         </div>
-    )
+      )}
+      <div className="flex flex-row box-border pr-2 mt-2 space-x-3 ">
+        <Image
+          className="basis-16 object-cover"
+          width="64"
+          height="64"
+          src={data.thumb}
+          alt={"a thumbnail with a photo of your plant!"}
+        />
+        {isExpanded && (
+          <div id={`${data.id}-main-info`} className="basis-auto ">
+            <p>
+              Scientific Name: <i>{data.sciName}</i>
+            </p>
+            <p>Nickname: {data.nickname}</p>
+            <p>Room: {data.room}</p>
+          </div>
+        )}
+        {!isExpanded && (
+          <div
+            id={`${data.id}-collapsed-view`}
+            className="align-bottom text-center space-y-1"
+          >
+            <p className="mt-4 md:mt-2">{data.nickname}</p>
+            <p className="italic">({abbrev(data.sciName)})</p>
+          </div>
+        )}
+      </div>
+      {isExpanded && (
+        <div id={`${data.id}-instructions`} className="py-4 pr-4 ">
+          <p className="pb-2">Watering: {data.instructions.water}</p>
+          <p className="pb-2">Light: {data.instructions.light}</p>
+        </div>
+      )}
+      {isExpanded && (
+        <div id={`${data.id}-feeds`} className="grid gap-2 grid-cols-2 pr-4">
+          <CardFeed
+            title="Last Watered"
+            buttonText="Water"
+            data={[
+              { text: "Eric", date: "April 4th", key: 0 },
+              { text: "Despina", date: "March 20th", key: 1 },
+              { text: "Flannery", date: "March 6th", key: 2 },
+            ]}
+          />
+          <CardFeed
+            title="Observations"
+            buttonText="Write Note"
+            data={[
+              {
+                text: '"Our child is thriving" ~Eric',
+                date: "April 4th",
+                key: 0,
+              },
+              { text: '"Do your dishes" ~Despina', date: "March 20th", key: 1 },
+            ]}
+          />
+        </div>
+      )}
+    </div>
+  );
 }
